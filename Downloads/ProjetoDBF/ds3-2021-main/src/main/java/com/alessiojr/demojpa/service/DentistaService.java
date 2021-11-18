4dfd6bbac3b31f0e78eb956402f8bbcc386a03c5
Luanna Gabriella <Luanna@LAPTOP-FED2AOAF>
2021-11-18 00:30:29 -0300

    tirei um espaço porque tava me incomodando

--- Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/service/DentistaService.java
+++ Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/service/DentistaService.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @Service
 public class DentistaService {
-     private final Logger log = LoggerFactory.getLogger(DentistaService.class);
+    private final Logger log = LoggerFactory.getLogger(DentistaService.class);
 
     private final DentistaRepository dentistaRepository;
 
