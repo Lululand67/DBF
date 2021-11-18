3c8f2346ad0aad5fe73d2a4a08eab97acf94f093
Luanna Gabriella <Luanna@LAPTOP-FED2AOAF>
2021-11-17 23:55:35 -0300

    Criei e implementei a classe dentistaservice

---
+++ Downloads/ds3-2021-main/ds3-2021-main/src/main/java/com/alessiojr/demojpa/service/DentistaService.java
@@ -0,0 +1,48 @@
+package com.alessiojr.demojpa.service;
+
+import com.alessiojr.demojpa.domain.Dentista;
+import com.alessiojr.demojpa.repository.DentistaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DentistaService {
+     private final Logger log = LoggerFactory.getLogger(DentistaService.class);
+
+    private final DentistaRepository dentistaRepository;
+
+    public DentistaService(DentistaRepository dentistaRepository) {
+        this.dentistaRepository = dentistaRepository;
+    }
+
+    public List<Dentista> findAllList(){
+        log.debug("Request to get All Dentista");
+        return dentistaRepository.findAll();
+    }
+
+    public Optional<Dentista> findOne(Long id) {
+        log.debug("Request to get Pessoa : {}", id);
+        return dentistaRepository.findById(id);
+    }
+
+    public void delete(Long id) {
+        log.debug("Request to delete Dentista : {}", id);
+        dentistaRepository.deleteById(id);
+    }
+
+    public Dentista save(Dentista dentista) {
+        log.debug("Request to save Dentista : {}", dentista);
+        dentista = dentistaRepository.save(dentista);
+        return dentista;
+    }
+
+    public List<Dentista> saveAll(List<Dentista> dentistas) {
+        log.debug("Request to save Dentista : {}", dentistas);
+        dentistas = dentistaRepository.saveAll(dentistas);
+        return dentistas;
+    }
+}
