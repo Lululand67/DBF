0c8ff6a9931d0f38d41a9efbe4b518cb2afa3605
Luanna Gabriella <Luanna@LAPTOP-FED2AOAF>
2021-11-18 00:06:38 -0300

    Criei e implementei a classe dentistaresource

---
+++ Downloads/ds3-2021-main/ds3-2021-main/src/main/java/com/alessiojr/demojpa/web/api/DentistaResource.java
@@ -0,0 +1,101 @@
+package com.alessiojr.demojpa.web.api;
+
+import com.alessiojr.demojpa.domain.Dentista;
+import com.alessiojr.demojpa.service.DentistaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/dentistas")
+public class DentistaResource {
+    
+    private final Logger log = LoggerFactory.getLogger(DentistaResource.class);
+
+    private final DentistaService dentistaService;
+
+    public DentistaResource(DentistaService dentistaService) {
+        this.dentistaService = dentistaService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Dentista> getDentista(@PathVariable Long id) {
+        log.debug("REST request to get Dentista : {}", id);
+        Optional<Dentista> dentista = dentistaService.findOne(id);
+        if(dentista.isPresent()) {
+            return ResponseEntity.ok().body(dentista.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Dentista>> getDentistas(){
+        List<Dentista> lista = dentistaService.findAllList();
+        if(lista.size() > 0) {
+            return ResponseEntity.ok().body(lista);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Dentista> updateDentista(@RequestBody Dentista dentista) throws URISyntaxException {
+        log.debug("REST request to update Dentista : {}", dentista);
+        if (dentista.getId() == null) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Invalid Dentista id null");
+        }
+        Dentista result = dentistaService.save(dentista);
+        return ResponseEntity.ok()
+                .body(result);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Dentista> createDentista(@RequestBody Dentista dentista) throws URISyntaxException {
+        log.debug("REST request to save Dentista : {}", dentista);
+        if (dentista.getId() != null) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Um novo dentista não pode ter um ID");
+        }
+        Dentista result = dentistaService.save(dentista);
+        return ResponseEntity.created(new URI("/api/dentistas/" + result.getId()))
+                .body(result);
+    }
+
+    @PostMapping(value = "/csv", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public List<Dentista> upload(@RequestPart("data") MultipartFile csv) throws IOException {
+        List<Dentista> savedNotes = new ArrayList<>();
+        List<Dentista> notes = new BufferedReader(
+                new InputStreamReader(Objects.requireNonNull(csv).getInputStream(), StandardCharsets.UTF_8)).lines()
+                .map(Dentista::parseNote).collect(Collectors.toList());
+        dentistaService.saveAll(notes).forEach(savedNotes::add);
+        return savedNotes;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDentista(@PathVariable Long id) {
+        log.debug("REST request to delete dentista : {}", id);
+
+        dentistaService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
