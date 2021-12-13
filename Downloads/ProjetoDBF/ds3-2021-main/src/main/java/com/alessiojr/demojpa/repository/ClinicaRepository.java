77aab05d1b74f24a03b036352ac3b91316e75a8d
Luanna Gabriella <Luanna@LAPTOP-FED2AOAF>
2021-12-13 12:25:23 -0300

    Adicionei buscar nos dois repositorios

--- Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/ClinicaRepository.java
+++ Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/ClinicaRepository.java
@@ -1,10 +1,13 @@
 package com.alessiojr.demojpa.repository;
 
 import com.alessiojr.demojpa.domain.Clinica;
+import com.alessiojr.demojpa.domain.Dentista;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClinicaRepository extends JpaRepository<Clinica, Long> {
     
+    Optional<Clinica> findByCNPJ(String cnpj);
 }
--- Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/DentistaRepository.java
+++ Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/DentistaRepository.java
@@ -1,10 +1,12 @@
 package com.alessiojr.demojpa.repository;
 
 import com.alessiojr.demojpa.domain.Dentista;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DentistaRepository extends JpaRepository<Dentista, Long> {
     
+    Optional<Dentista> findByCPF(String cpf);
 }
