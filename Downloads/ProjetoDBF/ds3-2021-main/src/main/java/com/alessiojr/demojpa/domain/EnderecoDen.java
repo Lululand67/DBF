cebd8e0d464d1f38d8df47a589f9a02bc10998c9
Luanna Gabriella <Luanna@LAPTOP-FED2AOAF>
2021-12-13 09:53:51 -0300

    Adicionei os dados das classes EnderecoCli e EnderecoDen

--- Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/domain/EnderecoDen.java
+++ Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/domain/EnderecoDen.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.alessiojr.demojpa.domain;
 
-/**
- *
- * @author Luanna
- */
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "table_enderecoD")
+
 public class EnderecoDen {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rua;
+    private String bairro;
+    private String cidade;
+    private String uf;
+
 }
