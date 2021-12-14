a1229ba92443295d78a370a3e7fe0f9e43ecc900
Luanna Gabriella <Luanna@LAPTOP-FED2AOAF>
2021-12-14 17:56:26 -0300

    Adicionei a classe pedido

---
+++ Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/domain/Pedido.java
@@ -0,0 +1,55 @@
+package com.alessiojr.demojpa.domain;
+import com.sun.istack.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "table_pedido")
+
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotNull
+    @Column(name = "nomeP", length = 64)
+    private String nomePaciente;
+    
+    @NotNull
+    @Column(name = "nomeD", length = 64)
+    private String nomeDentista;
+    
+    @ManyToOne
+    private Dentista dentista;
+    
+    private String descricao;
+    
+    @Temporal(TemporalType.DATE)
+    private Date dataEmissao;
+    
+    @Temporal(TemporalType.DATE)
+    private Date dataEntrega;
+    
+    private Float valor;
+    
+    private Boolean isActive;
+
+    public static Pedido parseNote(String line) {
+        String[] text = line.split(",");
+        Pedido note = new Pedido();
+        note.setId(Long.parseLong(text[0]));
+       // note.setNomePaciente(text[1]);
+       // note.setNomeDentista(text[1]);
+        return note;
+    }
+}
