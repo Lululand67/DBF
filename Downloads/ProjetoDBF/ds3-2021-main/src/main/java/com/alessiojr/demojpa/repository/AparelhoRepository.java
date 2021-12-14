3351e87e8b908fb43cba2923eec515a43c2091ca
Luanna Gabriella <Luanna@LAPTOP-FED2AOAF>
2021-12-14 18:09:59 -0300

    Adicionei o aparelhoRepository

---
+++ Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/AparelhoRepository.java
@@ -0,0 +1,9 @@
+package com.alessiojr.demojpa.repository;
+
+import com.alessiojr.demojpa.domain.Aparelho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface AparelhoRepository extends JpaRepository<Aparelho, Long>{
+    
+}
