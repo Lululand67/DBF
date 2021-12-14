dcb483c3f5684559b90ec89e3aebacce51124da8
Luanna Gabriella <Luanna@LAPTOP-FED2AOAF>
2021-12-14 18:36:29 -0300

    Tirei os novos arquivvos para achar o erro

--- Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/AparelhoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alessiojr.demojpa.repository;
-
-import com.alessiojr.demojpa.domain.Aparelho;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface AparelhoRepository extends JpaRepository<Aparelho, Long>{
-    
-}
--- Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/ClinicaRepository.java
+++ Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/ClinicaRepository.java
@@ -1,16 +1,11 @@
 package com.alessiojr.demojpa.repository;
 
 import com.alessiojr.demojpa.domain.Clinica;
-import com.alessiojr.demojpa.domain.Dentista;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClinicaRepository extends JpaRepository<Clinica, Long> {
     
-    Optional<Clinica> findByCNPJ(String cnpj);
 
 }
