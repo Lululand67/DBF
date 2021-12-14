2672db7c1da1206daafcef04956920e552b90494
Luanna Gabriella <Luanna@LAPTOP-FED2AOAF>
2021-12-14 18:41:16 -0300

    Adicionei a classe pedido e o repositório pedido

--- Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/PedidoRepository.java
+++ Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/PedidoRepository.java
@@ -1,1 +1,11 @@
+package com.alessiojr.demojpa.repository;
+
+import com.alessiojr.demojpa.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    
+}
 
