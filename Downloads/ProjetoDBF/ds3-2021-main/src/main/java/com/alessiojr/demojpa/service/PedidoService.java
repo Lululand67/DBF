6dfd76e1843bfb9e8e2e924712998e7ee0fd3804
Luanna Gabriella <Luanna@LAPTOP-FED2AOAF>
2022-01-11 10:24:20 -0300

    Criei e implementei a classe pedidoService

---
+++ Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/service/PedidoService.java
@@ -0,0 +1,48 @@
+package com.alessiojr.demojpa.service;
+
+import com.alessiojr.demojpa.domain.Pedido;
+import com.alessiojr.demojpa.repository.PedidoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+    private final Logger log = LoggerFactory.getLogger(PedidoService.class);
+
+    private final PedidoRepository pedidoRepository;
+
+    public PedidoService(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    public List<Pedido> findAllList(){
+        log.debug("Request to get All Pedido");
+        return pedidoRepository.findAll();
+    }
+
+    public Optional<Pedido> findOne(Long id) {
+        log.debug("Request to get Pedido : {}", id);
+        return pedidoRepository.findById(id);
+    }
+
+    public void delete(Long id) {
+        log.debug("Request to delete Clinica : {}", id);
+        pedidoRepository.deleteById(id);
+    }
+
+    public Pedido save(Pedido pedido) {
+        log.debug("Request to save Pedido : {}", pedido);
+        pedido = pedidoRepository.save(pedido);
+        return pedido;
+    }
+
+    public List<Pedido> saveAll(List<Pedido> pedidos) {
+        log.debug("Request to save Pedido : {}", pedidos);
+        pedidos = pedidoRepository.saveAll(pedidos);
+        return pedidos;
+    }
+}
