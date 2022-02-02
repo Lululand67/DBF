8145412f676f6247f51512591d979d66407bfbb0
Luanna Gabriella <Luanna@LAPTOP-FED2AOAF>
2022-02-02 03:02:25 -0300

    arrumei erros

--- Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/PedidoRepository.java
+++ Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/PedidoRepository.java
@@ -8,6 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-    Optional<Pedido> findByName(String nome);
 }
 
