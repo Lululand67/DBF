671ea0fd6a0719ed608db235be4855198516954f
Luanna Gabriella <Luanna@LAPTOP-FED2AOAF>
2022-02-02 01:43:21 -0300

    adicionei as relacoes nos repositorios

--- Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/PedidoRepository.java
+++ Downloads/ProjetoDBF/ds3-2021-main/src/main/java/com/alessiojr/demojpa/repository/PedidoRepository.java
@@ -1,11 +1,15 @@
 package com.alessiojr.demojpa.repository;
 
+import com.alessiojr.demojpa.domain.Aparelho;
+import com.alessiojr.demojpa.domain.Dentista;
 import com.alessiojr.demojpa.domain.Pedido;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-    
+    Optional<Dentista> findByName(String nome);
+    Optional<Aparelho> findByNameA(String nome);
 }
 
